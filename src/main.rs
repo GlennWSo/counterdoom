f32231a810142e9531743a4ccd791d95dbbc7321
gws <glennws@pm.me>
2025-03-06 11:40:40 +0100

    init pipes

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,16 @@
+use std::f32;
+
 use bevy::color::palettes::tailwind;
 use bevy::math::ops::atan2;
 use bevy::prelude::*;
+use bevy::utils::tracing::instrument::WithSubscriber;
+use bevy::window::WindowResolution;
 
 fn main() {
     let primary_window = Some(Window {
         title: "Counter DOOM".to_string(),
+        resolution: WindowResolution::new(512.0, 512.0),
+        resizable: false,
         ..Default::default()
     });
 
@@ -30,18 +36,72 @@ struct Bird {
     pub velocity: f32,
 }
 
+#[derive(Resource)]
+struct PipeMan {
+    pub pipe_image: Handle<Image>,
+    pub window_size: Vec2,
+}
+
+impl PipeMan {
+    const PIPE_HIEGHT: f32 = 144.0;
+    const PIPE_WIDTH: f32 = 18.0;
+}
+
 const SCROLL_SPEED: f32 = 10.0;
 
 impl Bird {
-    const GRAVITY: f32 = -3.0;
+    const GRAVITY: f32 = -0.0;
     const FLAP_DV: f32 = 1.0;
+    const SIZE_PIXEL_Y: f32 = 8.0;
+    const SIZE_Y: f32 = Bird::SIZE_PIXEL_Y * PIXEL_RATIO;
 }
 
 const PIXEL_RATIO: f32 = 4.0;
 
-fn setup_level(mut cmds: Commands, asset_server: Res<AssetServer>) {
+#[derive(Component)]
+struct Obsticle {
+    gap: f32,
+}
+
+fn spawn_obsticle(pos: Vec2, gap: f32, cmds: &mut Commands, image: Handle<Image>) {
+    let y = -PipeMan::PIPE_HIEGHT / 2.0 * PIXEL_RATIO + pos.y - gap / 2.0;
+
+    let translation = Vec3 {
+        x: 0.0 + pos.x,
+        y,
+        z: 0.0,
+    };
+    let transform = Transform::from_translation(translation).with_scale(Vec3::splat(PIXEL_RATIO));
+
+    let mut transform2 = transform.clone();
+    transform2.rotate_around(Vec3::ZERO, Quat::from_rotation_z(std::f32::consts::PI));
+    transform2.scale.x *= -1.0;
+
+    let sprite = dbg!(Sprite {
+        image,
+        ..Default::default()
+    });
+    let bundle1 = (Obsticle { gap }, sprite.clone(), transform);
+    let bundle2 = (Obsticle { gap }, sprite, transform2);
+    cmds.spawn_batch([bundle1, bundle2]);
+}
+
+fn setup_level(winq: Query<&Window>, mut cmds: Commands, asset_server: Res<AssetServer>) {
     cmds.insert_resource(ClearColor(Color::from(tailwind::TEAL_400)));
     cmds.spawn(Camera2d::default());
+
+    let pipe_image: Handle<Image> = asset_server.load("pipe.png");
+    let resource = PipeMan {
+        pipe_image: pipe_image.clone(),
+        window_size: winq.single().resolution.size(),
+    };
+    cmds.insert_resource(resource);
+
+    let gap = Bird::SIZE_Y * 2.0;
+    spawn_obsticle(Vec2::ZERO, gap, &mut cmds, pipe_image);
+
+    let mut transform = Transform::IDENTITY.with_scale(Vec3::splat(PIXEL_RATIO));
+    // transform.translation.y = Bird::BIRD_PX_Y / 2;
 
     let player_bundle = (
         Player,
@@ -50,7 +110,7 @@ fn setup_level(mut cmds: Commands, asset_server: Res<AssetServer>) {
             image: asset_server.load("bird.png"),
             ..Default::default()
         },
-        Transform::IDENTITY.with_scale(Vec3::splat(PIXEL_RATIO)),
+        transform,
     );
     cmds.spawn(player_bundle);
 }
